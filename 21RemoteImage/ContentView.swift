2b6c1c23d0eb875b8b8365d86fcdb056d7d23ae3
AlejandroSZIOSA <ggato33@gmail.com>
2023-10-28 14:23:19 +0200

    Initial Commit

---
+++ 21RemoteImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  21RemoteImage
+//
+//  Created by gabriel Sazo on 2023-10-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 21RemoteImage/_1RemoteImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1RemoteImageApp.swift
+//  21RemoteImage
+//
+//  Created by gabriel Sazo on 2023-10-28.
+//
+
+import SwiftUI
+
+@main
+struct _1RemoteImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
